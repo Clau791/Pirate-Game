3a7dc944c11cbdd341a157d561a06804f7fa577f
Clau <klau2015.cc@gmail.com>
2025-03-14 16:42:13 +0200

    Commit #2: adaugare meniu

---
+++ meniu_select_nivel.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_nivel1_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_nivel2_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/new_level.tscn")
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
--- scenes/area_2d.gd
+++ scenes/area_2d.gd
@@ -9,6 +9,7 @@ func _on_body_entered(body):
 	#easy teleport
 	if body.name =="player":
 		get_tree().change_scene_to_file("res://scenes/new_level.tscn")
+		
 			#Apelăm funcția de tranziție din CanvasLayer
 	#var canvas_layer = get_node("../CanvasLayer") # Adjust path based on your node hierarchy
 	#await canvas_layer.start_transition_to_scene(target_scene)
