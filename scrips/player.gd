88ce9f7b1f39a6a7105995c1e8e443eb43419034
Clau <klau2015.cc@gmail.com>
2025-03-15 22:49:49 +0200

    Commit #4 : added new level

--- scrips/player.gd
+++ scrips/player.gd
@@ -3,9 +3,20 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
+const CLIMB_SPEED = 150
 
 @onready var sprite = $sprite
 
+var on_ladder = false
+
+#urcare verticala scara
+func _on_ladder_body_entered(body: Node2D) -> void:
+	
+	on_ladder = true
+
+func _on_ladder_body_exited(body: Node2D) -> void:
+	on_ladder = false
+		
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	
@@ -15,10 +26,20 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+	# Cățărare pe scară
+	if on_ladder:
+		velocity.y = 0
+		if Input.is_action_pressed("ui_up"):
+			velocity.y = -CLIMB_SPEED
+		if Input.is_action_pressed("ui_down"):
+			velocity.y = CLIMB_SPEED
+			
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
 	if direction :
 		
 		velocity.x = direction * SPEED
