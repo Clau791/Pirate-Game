04e8c91a79a334c7334ed44d70c40da600d0b0a4
Clau <klau2015.cc@gmail.com>
2025-03-17 22:35:37 +0200

    Commit #1 : Setting up the player

--- scrips/player.gd
+++ scrips/player.gd
@@ -8,6 +8,7 @@ const CLIMB_SPEED = 150
 @onready var Attack_Hitbox = $Attack_Hitbox
 @onready var Attack_Cooldown = $Attack_Cooldown
 @onready var sprite = $sprite
+@onready var attacking = $Attack_Animation
 
 var on_ladder = false
 
@@ -15,6 +16,7 @@ var max_health = 100
 var health = max_health
 var damage = 20  # Cât damage dă jucătorul
 var can_attack = true
+var is_attacking = false # flag pentru a nu se intrerupe animatiile
 
 # functii pentru health/ take damage
 func _on_ready() -> void: # incepe cu maxim health
@@ -68,23 +70,29 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
-	if direction :
+	if direction and not is_attacking :
 		
 		velocity.x = direction * SPEED
 		
-		if direction > 0:  # >0 adica se misca pozitiv pe x, animatie normala de walk
+		if direction > 0 :  # >0 adica se misca pozitiv pe x, animatie normala de walk
+			sprite.flip_h = false; # flip imagine horizontal
 			sprite.play("walk")
 		else:
-			sprite.play("backwards")
-	else:
+			sprite.flip_h = true;
+			sprite.play("walk")
+	elif not is_attacking:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		sprite.play("idle")	
 	
 	
 	if Input.is_action_pressed("ui_attack") and can_attack:
 		#play here animation
-		can_attack = false
-		$Attack_Cooldown.start()
+		can_attack = false # pentru a nu se face spam de attack
+		is_attacking = true # pentru a nu interveni alte animatii
+		attacking.start()
+		sprite.play("attack")
+		Attack_Cooldown.start()
+		
 		if Attack_Hitbox.is_colliding():
 			print("colliding")
 			var enemy = Attack_Hitbox.get_collider()
@@ -93,9 +101,14 @@ func _physics_process(delta: float) -> void:
 			if enemy and ( enemy.is_in_group("Enemies") or enemy.is_in_group("Enemy") ):
 				print("Atac")
 				enemy.take_damage(damage)
-		
+
+
 	move_and_slide()
 
 # pentru a pune un cooldown pe attack
 func _on_cooldown_timeout() -> void:
 	can_attack = true
+	
+
+func _on_attack_animation_timeout() -> void:
+	is_attacking = false # Replace with function body.
