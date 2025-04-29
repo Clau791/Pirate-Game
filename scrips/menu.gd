2f20153676e9afb659c93b41d167c74cf6086ef0
Rares Igescu <raresigescu@gmail.com>
2025-04-29 15:41:48 +0300

    Create design for level menu

--- scrips/menu.gd
+++ scrips/menu.gd
@@ -5,11 +5,13 @@ var is_paused := false
 @onready var main_menu = $MainMenu  
 @onready var help_menu = $HelpMenu   
 @onready var settings_menu = $SettingsMenu
+@onready var level_menu = $LevelMenu
 
 func _ready():
 	hide()  
 	help_menu.hide()
 	settings_menu.hide()
+	level_menu.hide()
 	set_process_mode(Node.PROCESS_MODE_ALWAYS)  
 
 func _unhandled_input(event):
@@ -25,20 +27,30 @@ func toggle_pause_menu():
 		main_menu.show()
 		settings_menu.hide()
 		help_menu.hide()
+		level_menu.hide()
 		
 
 func _on_help_button_pressed():
 	main_menu.hide()
 	settings_menu.hide()
+	level_menu.hide()
 	help_menu.show()
 
 func _on_back_button_pressed():
 	help_menu.hide()
 	settings_menu.hide()
+	level_menu.hide()
 	main_menu.show()
 
 func _on_settings_button_pressed():
 	settings_menu.show()
+	help_menu.hide()
+	main_menu.hide()
+	level_menu.hide()
+
+func _on_level_button_pressed():
+	level_menu.show()
+	settings_menu.hide()
 	help_menu.hide()
 	main_menu.hide()
 
